0ac24c913cb891afb199ca710ef30944c987b49a
FlattyPatty <berbalaf.45@gmail.com>
2023-12-30 12:17:42 +0100

    Creating new set is created, and appending and modifying word pairs are working.

---
+++ src/newset.py
@@ -0,0 +1,620 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'new_set.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractItemView, QApplication, QHBoxLayout, QHeaderView,
+    QLabel, QLineEdit, QMainWindow, QPushButton,
+    QSizePolicy, QSpacerItem, QTableView, QVBoxLayout,
+    QWidget)
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(500, 600)
+        MainWindow.setMinimumSize(QSize(500, 600))
+        MainWindow.setMaximumSize(QSize(500, 600))
+        MainWindow.setStyleSheet(u"/*\n"
+"Material Dark Style Sheet for QT Applications\n"
+"Author: Jaime A. Quiroga P.\n"
+"Inspired on https://github.com/jxfwinter/qt-material-stylesheet\n"
+"Company: GTRONICK\n"
+"Last updated: 04/12/2018, 15:00.\n"
+"Available at: https://github.com/GTRONICK/QSS/blob/master/MaterialDark.qss\n"
+"*/\n"
+"QMainWindow {\n"
+"	background-color:#1e1d23;\n"
+"}\n"
+"QDialog {\n"
+"	background-color:#1e1d23;\n"
+"}\n"
+"QColorDialog {\n"
+"	background-color:#1e1d23;\n"
+"}\n"
+"QTextEdit {\n"
+"	background-color:#1e1d23;\n"
+"	color: #a9b7c6;\n"
+"}\n"
+"QPlainTextEdit {\n"
+"	selection-background-color:#007b50;\n"
+"	background-color:#1e1d23;\n"
+"	border-style: solid;\n"
+"	border-top-color: transparent;\n"
+"	border-right-color: transparent;\n"
+"	border-left-color: transparent;\n"
+"	border-bottom-color: transparent;\n"
+"	border-width: 1px;\n"
+"	color: #a9b7c6;\n"
+"}\n"
+"QPushButton{\n"
+"	border-style: solid;\n"
+"	border-top-color: transparent;\n"
+"	border-right-color: transparent;\n"
+"	border-left-color: transparent;\n"
+"	border-botto"
+                        "m-color: transparent;\n"
+"	border-width: 1px;\n"
+"	border-style: solid;\n"
+"	color: #a9b7c6;\n"
+"	padding: 2px;\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"QPushButton::default{\n"
+"	border-style: inset;\n"
+"	border-top-color: transparent;\n"
+"	border-right-color: transparent;\n"
+"	border-left-color: transparent;\n"
+"	border-bottom-color: #04b97f;\n"
+"	border-width: 1px;\n"
+"	color: #a9b7c6;\n"
+"	padding: 2px;\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"QToolButton {\n"
+"	border-style: solid;\n"
+"	border-top-color: transparent;\n"
+"	border-right-color: transparent;\n"
+"	border-left-color: transparent;\n"
+"	border-bottom-color: #04b97f;\n"
+"	border-bottom-width: 1px;\n"
+"	border-style: solid;\n"
+"	color: #a9b7c6;\n"
+"	padding: 2px;\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"QToolButton:hover{\n"
+"	border-style: solid;\n"
+"	border-top-color: transparent;\n"
+"	border-right-color: transparent;\n"
+"	border-left-color: transparent;\n"
+"	border-bottom-color: #37efba;\n"
+"	border-bottom-width: 2px;\n"
+"	border-style: solid"
+                        ";\n"
+"	color: #FFFFFF;\n"
+"	padding-bottom: 1px;\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"QPushButton:hover{\n"
+"	border-style: solid;\n"
+"	border-top-color: transparent;\n"
+"	border-right-color: transparent;\n"
+"	border-left-color: transparent;\n"
+"	border-bottom-color: #37efba;\n"
+"	border-bottom-width: 1px;\n"
+"	border-style: solid;\n"
+"	color: #FFFFFF;\n"
+"	padding-bottom: 2px;\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"QPushButton:pressed{\n"
+"	border-style: solid;\n"
+"	border-top-color: transparent;\n"
+"	border-right-color: transparent;\n"
+"	border-left-color: transparent;\n"
+"	border-bottom-color: #37efba;\n"
+"	border-bottom-width: 2px;\n"
+"	border-style: solid;\n"
+"	color: #37efba;\n"
+"	padding-bottom: 1px;\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"QPushButton:disabled{\n"
+"	border-style: solid;\n"
+"	border-top-color: transparent;\n"
+"	border-right-color: transparent;\n"
+"	border-left-color: transparent;\n"
+"	border-bottom-color: #808086;\n"
+"	border-bottom-width: 2px;\n"
+"	border-style: solid;\n"
+"	co"
+                        "lor: #808086;\n"
+"	padding-bottom: 1px;\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"QLineEdit {\n"
+"	border-width: 1px; border-radius: 4px;\n"
+"	border-color: rgb(58, 58, 58);\n"
+"	border-style: inset;\n"
+"	padding: 0 8px;\n"
+"	color: #a9b7c6;\n"
+"	background:#1e1d23;\n"
+"	selection-background-color:#007b50;\n"
+"	selection-color: #FFFFFF;\n"
+"}\n"
+"QLabel {\n"
+"	color: #a9b7c6;\n"
+"}\n"
+"QLCDNumber {\n"
+"	color: #37e6b4;\n"
+"}\n"
+"QProgressBar {\n"
+"	text-align: center;\n"
+"	color: rgb(240, 240, 240);\n"
+"	border-width: 1px; \n"
+"	border-radius: 10px;\n"
+"	border-color: rgb(58, 58, 58);\n"
+"	border-style: inset;\n"
+"	background-color:#1e1d23;\n"
+"}\n"
+"QProgressBar::chunk {\n"
+"	background-color: #04b97f;\n"
+"	border-radius: 5px;\n"
+"}\n"
+"QMenuBar {\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"QMenuBar::item {\n"
+"	color: #a9b7c6;\n"
+"  	spacing: 3px;\n"
+"  	padding: 1px 4px;\n"
+"  	background: #1e1d23;\n"
+"}\n"
+"\n"
+"QMenuBar::item:selected {\n"
+"  	background:#1e1d23;\n"
+"	color: #FFFFFF;\n"
+"}\n"
+"QMenu::item:s"
+                        "elected {\n"
+"	border-style: solid;\n"
+"	border-top-color: transparent;\n"
+"	border-right-color: transparent;\n"
+"	border-left-color: #04b97f;\n"
+"	border-bottom-color: transparent;\n"
+"	border-left-width: 2px;\n"
+"	color: #FFFFFF;\n"
+"	padding-left:15px;\n"
+"	padding-top:4px;\n"
+"	padding-bottom:4px;\n"
+"	padding-right:7px;\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"QMenu::item {\n"
+"	border-style: solid;\n"
+"	border-top-color: transparent;\n"
+"	border-right-color: transparent;\n"
+"	border-left-color: transparent;\n"
+"	border-bottom-color: transparent;\n"
+"	border-bottom-width: 1px;\n"
+"	border-style: solid;\n"
+"	color: #a9b7c6;\n"
+"	padding-left:17px;\n"
+"	padding-top:4px;\n"
+"	padding-bottom:4px;\n"
+"	padding-right:7px;\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"QMenu{\n"
+"	background-color:#1e1d23;\n"
+"}\n"
+"QTabWidget {\n"
+"	color:rgb(0,0,0);\n"
+"	background-color:#1e1d23;\n"
+"}\n"
+"QTabWidget::pane {\n"
+"		border-color: rgb(77,77,77);\n"
+"		background-color:#1e1d23;\n"
+"		border-style: solid;\n"
+"		border-"
+                        "width: 1px;\n"
+"    	border-radius: 6px;\n"
+"}\n"
+"QTabBar::tab {\n"
+"	border-style: solid;\n"
+"	border-top-color: transparent;\n"
+"	border-right-color: transparent;\n"
+"	border-left-color: transparent;\n"
+"	border-bottom-color: transparent;\n"
+"	border-bottom-width: 1px;\n"
+"	border-style: solid;\n"
+"	color: #808086;\n"
+"	padding: 3px;\n"
+"	margin-left:3px;\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"QTabBar::tab:selected, QTabBar::tab:last:selected, QTabBar::tab:hover {\n"
+"  	border-style: solid;\n"
+"	border-top-color: transparent;\n"
+"	border-right-color: transparent;\n"
+"	border-left-color: transparent;\n"
+"	border-bottom-color: #04b97f;\n"
+"	border-bottom-width: 2px;\n"
+"	border-style: solid;\n"
+"	color: #FFFFFF;\n"
+"	padding-left: 3px;\n"
+"	padding-bottom: 2px;\n"
+"	margin-left:3px;\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"\n"
+"QCheckBox {\n"
+"	color: #a9b7c6;\n"
+"	padding: 2px;\n"
+"}\n"
+"QCheckBox:disabled {\n"
+"	color: #808086;\n"
+"	padding: 2px;\n"
+"}\n"
+"\n"
+"QCheckBox:hover {\n"
+"	border-radius:4px;"
+                        "\n"
+"	border-style:solid;\n"
+"	padding-left: 1px;\n"
+"	padding-right: 1px;\n"
+"	padding-bottom: 1px;\n"
+"	padding-top: 1px;\n"
+"	border-width:1px;\n"
+"	border-color: rgb(87, 97, 106);\n"
+"	background-color:#1e1d23;\n"
+"}\n"
+"QCheckBox::indicator:checked {\n"
+"\n"
+"	height: 10px;\n"
+"	width: 10px;\n"
+"	border-style:solid;\n"
+"	border-width: 1px;\n"
+"	border-color: #04b97f;\n"
+"	color: #a9b7c6;\n"
+"	background-color: #04b97f;\n"
+"}\n"
+"QCheckBox::indicator:unchecked {\n"
+"\n"
+"	height: 10px;\n"
+"	width: 10px;\n"
+"	border-style:solid;\n"
+"	border-width: 1px;\n"
+"	border-color: #04b97f;\n"
+"	color: #a9b7c6;\n"
+"	background-color: transparent;\n"
+"}\n"
+"QRadioButton {\n"
+"	color: #a9b7c6;\n"
+"	background-color: #1e1d23;\n"
+"	padding: 1px;\n"
+"}\n"
+"QRadioButton::indicator:checked {\n"
+"	height: 10px;\n"
+"	width: 10px;\n"
+"	border-style:solid;\n"
+"	border-radius:5px;\n"
+"	border-width: 1px;\n"
+"	border-color: #04b97f;\n"
+"	color: #a9b7c6;\n"
+"	background-color: #04b97f;\n"
+"}\n"
+"QRadioButton::indicator:!checked {\n"
+""
+                        "	height: 10px;\n"
+"	width: 10px;\n"
+"	border-style:solid;\n"
+"	border-radius:5px;\n"
+"	border-width: 1px;\n"
+"	border-color: #04b97f;\n"
+"	color: #a9b7c6;\n"
+"	background-color: transparent;\n"
+"}\n"
+"QStatusBar {\n"
+"	color:#027f7f;\n"
+"}\n"
+"QSpinBox {\n"
+"	color: #a9b7c6;	\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"QDoubleSpinBox {\n"
+"	color: #a9b7c6;	\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"QTimeEdit {\n"
+"	color: #a9b7c6;	\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"QDateTimeEdit {\n"
+"	color: #a9b7c6;	\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"QDateEdit {\n"
+"	color: #a9b7c6;	\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"QComboBox {\n"
+"	color: #a9b7c6;	\n"
+"	background: #1e1d23;\n"
+"}\n"
+"QComboBox:editable {\n"
+"	background: #1e1d23;\n"
+"	color: #a9b7c6;\n"
+"	selection-background-color: #1e1d23;\n"
+"}\n"
+"QComboBox QAbstractItemView {\n"
+"	color: #a9b7c6;	\n"
+"	background: #1e1d23;\n"
+"	selection-color: #FFFFFF;\n"
+"	selection-background-color: #1e1d23;\n"
+"}\n"
+"QComboBox:!editable:on, QComboBox::drop-do"
+                        "wn:editable:on {\n"
+"	color: #a9b7c6;	\n"
+"	background: #1e1d23;\n"
+"}\n"
+"QFontComboBox {\n"
+"	color: #a9b7c6;	\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"QToolBox {\n"
+"	color: #a9b7c6;\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"QToolBox::tab {\n"
+"	color: #a9b7c6;\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"QToolBox::tab:selected {\n"
+"	color: #FFFFFF;\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"QScrollArea {\n"
+"	color: #FFFFFF;\n"
+"	background-color: #1e1d23;\n"
+"}\n"
+"QSlider::groove:horizontal {\n"
+"	height: 5px;\n"
+"	background: #04b97f;\n"
+"}\n"
+"QSlider::groove:vertical {\n"
+"	width: 5px;\n"
+"	background: #04b97f;\n"
+"}\n"
+"QSlider::handle:horizontal {\n"
+"	background: qlineargradient(x1:0, y1:0, x2:1, y2:1, stop:0 #b4b4b4, stop:1 #8f8f8f);\n"
+"	border: 1px solid #5c5c5c;\n"
+"	width: 14px;\n"
+"	margin: -5px 0;\n"
+"	border-radius: 7px;\n"
+"}\n"
+"QSlider::handle:vertical {\n"
+"	background: qlineargradient(x1:1, y1:1, x2:0, y2:0, stop:0 #b4b4b4, stop:1 #8f8f8f);\n"
+"	border: 1px solid #5c5c5c;\n"
+"	height: 14"
+                        "px;\n"
+"	margin: 0 -5px;\n"
+"	border-radius: 7px;\n"
+"}\n"
+"QSlider::add-page:horizontal {\n"
+"    background: white;\n"
+"}\n"
+"QSlider::add-page:vertical {\n"
+"    background: white;\n"
+"}\n"
+"QSlider::sub-page:horizontal {\n"
+"    background: #04b97f;\n"
+"}\n"
+"QSlider::sub-page:vertical {\n"
+"    background: #04b97f;\n"
+"}")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.horizontalLayout = QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalSpacer_4 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer_4)
+
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_3.addItem(self.horizontalSpacer)
+
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer)
+
+        self.label_3 = QLabel(self.centralwidget)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setStyleSheet(u"font: 18pt \"MS Sans Serif\";")
+        self.label_3.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label_3)
+
+        self.title_edit = QLineEdit(self.centralwidget)
+        self.title_edit.setObjectName(u"title_edit")
+        self.title_edit.setStyleSheet(u"font: 16pt \"MS Sans Serif\";")
+
+        self.verticalLayout.addWidget(self.title_edit)
+
+        self.verticalSpacer_5 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer_5)
+
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setStyleSheet(u"font: 18pt \"MS Sans Serif\";")
+        self.label_2.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label_2)
+
+        self.word_edit = QLineEdit(self.centralwidget)
+        self.word_edit.setObjectName(u"word_edit")
+        self.word_edit.setStyleSheet(u"font: 16pt \"MS Sans Serif\";")
+
+        self.verticalLayout.addWidget(self.word_edit)
+
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setStyleSheet(u"font: 18pt \"MS Sans Serif\";")
+        self.label.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.label)
+
+        self.translation_edit = QLineEdit(self.centralwidget)
+        self.translation_edit.setObjectName(u"translation_edit")
+        self.translation_edit.setStyleSheet(u"font: 16pt \"MS Sans Serif\";")
+
+        self.verticalLayout.addWidget(self.translation_edit)
+
+        self.verticalSpacer_4 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer_4)
+
+        self.append_button = QPushButton(self.centralwidget)
+        self.append_button.setObjectName(u"append_button")
+        self.append_button.setMinimumSize(QSize(0, 40))
+        self.append_button.setFocusPolicy(Qt.NoFocus)
+        self.append_button.setStyleSheet(u"font: 16pt \"MS Sans Serif\";")
+
+        self.verticalLayout.addWidget(self.append_button)
+
+        self.modify_button = QPushButton(self.centralwidget)
+        self.modify_button.setObjectName(u"modify_button")
+        self.modify_button.setMinimumSize(QSize(0, 40))
+        self.modify_button.setFocusPolicy(Qt.NoFocus)
+        self.modify_button.setStyleSheet(u"font: 16pt \"MS Sans Serif\";")
+
+        self.verticalLayout.addWidget(self.modify_button)
+
+        self.delete_button = QPushButton(self.centralwidget)
+        self.delete_button.setObjectName(u"delete_button")
+        self.delete_button.setMinimumSize(QSize(0, 40))
+        self.delete_button.setFocusPolicy(Qt.NoFocus)
+        self.delete_button.setStyleSheet(u"font: 16pt \"MS Sans Serif\";")
+
+        self.verticalLayout.addWidget(self.delete_button)
+
+        self.save_button = QPushButton(self.centralwidget)
+        self.save_button.setObjectName(u"save_button")
+        self.save_button.setMinimumSize(QSize(0, 40))
+        self.save_button.setFocusPolicy(Qt.NoFocus)
+        self.save_button.setStyleSheet(u"font: 16pt \"MS Sans Serif\";")
+
+        self.verticalLayout.addWidget(self.save_button)
+
+        self.verticalSpacer_3 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer_3)
+
+        self.exit_button = QPushButton(self.centralwidget)
+        self.exit_button.setObjectName(u"exit_button")
+        self.exit_button.setMinimumSize(QSize(0, 40))
+        self.exit_button.setFocusPolicy(Qt.NoFocus)
+        self.exit_button.setStyleSheet(u"font: 16pt \"MS Sans Serif\";")
+
+        self.verticalLayout.addWidget(self.exit_button)
+
+        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer_2)
+
+
+        self.horizontalLayout_3.addLayout(self.verticalLayout)
+
+        self.horizontalSpacer_3 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_3.addItem(self.horizontalSpacer_3)
+
+
+        self.horizontalLayout.addLayout(self.horizontalLayout_3)
+
+        self.horizontalSpacer_5 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer_5)
+
+        self.tableView = QTableView(self.centralwidget)
+        self.tableView.setObjectName(u"tableView")
+        self.tableView.setFocusPolicy(Qt.NoFocus)
+        self.tableView.setStyleSheet(u"QTableView{\n"
+"	background-color: #1e1d23;\n"
+"	border-color: #a9b7c6;\n"
+"	border-width: 1px;\n"
+"	border-style: inset;\n"
+"	alternate-row-colors: false;\n"
+"	font: 75 15pt \"MS Sans Serif\";\n"
+"	selection-color: #37efba;\n"
+"	selection-background-color: #1e1d23;\n"
+"	\n"
+"}\n"
+"QTableView::item{\n"
+"	color: #a9b7c6;\n"
+"	align: center;\n"
+"\n"
+"\n"
+"}\n"
+"QTableView::item:selected{\n"
+"	border-top-color: transparent;\n"
+"	border-right-color: transparent;\n"
+"	border-left-color: transparent;\n"
+"	border-bottom-color: #37efba;\n"
+"	border-bottom-width: 2px;\n"
+"	border-style: solid;\n"
+"	color: #37efba;\n"
+"	padding-bottom: 1px;\n"
+"	background: transparent;\n"
+"}\n"
+"")
+        self.tableView.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.tableView.setDragDropOverwriteMode(False)
+        self.tableView.setSelectionMode(QAbstractItemView.ExtendedSelection)
+
+        self.horizontalLayout.addWidget(self.tableView)
+
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer_2)
+
+        self.horizontalLayout.setStretch(1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"C\u00edm", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Sz\u00f3", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"Ford\u00edt\u00e1s", None))
+        self.append_button.setText(QCoreApplication.translate("MainWindow", u"Hozz\u00e1ad\u00e1s", None))
+        self.modify_button.setText(QCoreApplication.translate("MainWindow", u"M\u00f3dos\u00edt\u00e1s", None))
+        self.delete_button.setText(QCoreApplication.translate("MainWindow", u"T\u00f6rl\u00e9s", None))
+        self.save_button.setText(QCoreApplication.translate("MainWindow", u"Ment\u00e9s", None))
+        self.exit_button.setText(QCoreApplication.translate("MainWindow", u"M\u00e9gse", None))
+    # retranslateUi
+
